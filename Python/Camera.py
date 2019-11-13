93d8017ed54971337af89adf36c8b53934de4740
Mikkel Kristensen <nicemikkel@hotmail.com>
2019-11-13 09:24:08 +0100

    Merged with master

--- Python/Camera.py
+++ Python/Camera.py
@@ -1,7 +1,9 @@
 import cv2
+import numpy as np
 import math
 
 # FPS rate needs to be faster - to be continued ;-)
+
 
 class Camera:
 
@@ -14,46 +16,37 @@ class Camera:
         # .read() gives us a tuple, where index 0 is a boolean, and index 1 is the image data.
         frame = self.__camera.read()[1]
 
-        w, h, c = frame.shape
+        width, height, channels = frame.shape
 
-        def hsv():
-            for x in range(w):
-                for y in range(h):
-                    blue = int(frame[x, y, 0])
-                    green = int(frame[x, y, 1])
-                    red = int(frame[x, y, 2])
+        for x in range(width):
+            for y in range(height):
+                blue = int(frame[x, y, 0])
+                green = int(frame[x, y, 1])
+                red = int(frame[x, y, 2])
 
-                    denom = (red - green) ** 2 + (red - blue) * (green - blue)
+                denominator = (red - green) ** 2 + (red - blue) * (green - blue)
 
-                    # Calculations for hue, saturation and intensity
-                    # Hue
-                    # to avoid divide by zero exception, an exception was made
-                    if denom is not 0:
-                        θ = math.acos((((red - green) + (red - blue)) / 2) / (
-                            math.sqrt((red - green) ** 2 + (red - blue) * (green - blue))))
+                # Calculations for hue, saturation and intensity
+                # Hue
+                # to avoid divide by zero exception, an exception was made
+                if denominator is not 0:
+                    θ = math.acos((((red - green) + (red - blue)) / 2) / (
+                        math.sqrt((red - green) ** 2 + (red - blue) * (green - blue))))
 
-                        if green >= blue:
-                            hue = θ
-                        else:
-                            hue = 360 - θ
+                    if green >= blue:
+                        hue = θ
                     else:
-                        hue = 0
+                        hue = 360 - θ
+                else:
+                    hue = 0
 
-                    # Saturation
-                    # to avoid divide by zero exception, a small number is added
-                    saturation = (1 - (3 * (min(red, green, blue))) / (red + green + blue + 0.0000001)) * 255
+                # Saturation
+                # to avoid divide by zero exception, a small number is added
+                saturation = (1 - (3 * (min(red, green, blue))) / (red + green + blue + 0.0000001)) * 255
 
-                    # Value
-                    value = max(red, green, blue)
+                # Value
+                value = max(red, green, blue)
 
-                    frame[x, y] = hue, saturation, value
+                frame[x, y] = hue, saturation, value
 
-            # Output img with window name as 'HSV'
-            cv2.imshow('HSV', frame)
-
-        hsv()
-
-        if frame is not None:
-            return frame
-        else:
-            print("ERROR: getFrame() returned empty frame.")
+        return frame
