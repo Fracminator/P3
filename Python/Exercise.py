9bb748b58c8f94c99b4356271a6cd864077d69eb
Frederik Carlsen <fcarls18@student.aau.dk>
2019-11-18 13:38:04 +0100

    Score

--- Python/Exercise.py
+++ Python/Exercise.py
@@ -7,17 +7,20 @@ class Exercise(Scene):
     def __init__(self, coordinates, sceneManager, camera):
         super().__init__(sceneManager, camera)  # Call the initializing function of the superclass, aka Scene.
         self.coordinates = coordinates
+        self.score = 0
 
     # Overrides superclass update() function
     def update(self):
         frame = self.camera.getFrameHSV()
+        cv2.imshow('dinmor', frame)
         mask = self.camera.Masking(frame)
-        cv2.imshow("Frame", frame)
-        cv2.imshow("Mask", mask)
-
-        # cv2.imshow("frame", frame)
+        cv2.imshow("Mask", self.camera.Masking(frame))
+        avgx, avgy = self.camera.getCenterPixel(mask)
+        self.validate(avgx, avgy, 100, 100)
+        print(self.score)
         cv2.waitKey(1)
         # self.sceneManager.setActiveScene("Menu")  # sceneManager was inherited from the superclass.
 
-    def validate(self, coordinates, step):
-        place = "holder"
+    def validate(self, avgx, avgy, ptx, pty):
+        if avgx > ptx - 25 and avgx < ptx + 25 and avgy > pty - 25 and avgy < ptx + 25:
+            self.score += 1
