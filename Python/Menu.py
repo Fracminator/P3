8090a68020405033a6f3c6230b54bfb6081207d7
Mikkel Kristensen <nicemikkel@hotmail.com>
2019-11-20 12:33:28 +0100

    Added customizability to mainloop

--- Python/Menu.py
+++ Python/Menu.py
@@ -3,27 +3,30 @@ import tkinter as tk
 from tkinter import*
 
 
-
-
 class Menu(Scene):
 
     def __init__(self, sceneManager, camera):
         super().__init__(sceneManager, camera)
-        self.height=720
-        self.width=1280
+        self.camera = camera
+        self.width = 1280
+        self.height = 720
+        self.root = tk.Tk()
+        self.img = PhotoImage(file="exe1.png")
+        self.img2 = PhotoImage(file="exe2.png")
+        self.setupScene()
+
+    def setupScene(self):
+        canvas = Canvas(self.root, height=self.height, width=self.width, bg="white")
+        canvas.pack()
+        label1 = Label(self.root, text="Welcome to our shoulder rehabilitation program!", bg="white", font=("times", 20))
+        label1.place(x=400, y=40)
+        label2 = Label(self.root, text="Please select an exercise, by moving your hand:", bg="white", font=("times", 20))
+        label2.place(x=400, y=75)
+        canvas.create_image(170, 260, anchor=NW, image=self.img)
+        canvas.create_image(840, 225, anchor=NW, image=self.img2)
+
     # Overrides superclass update() function
     def update(self):
-        print("test")
-        root = tk.Tk()
-        canvas = Canvas(root, height=self.height, width=self.width,bg="white")
-        canvas.pack()
-        label1 = Label(root, text="Welcome to our shoulder rehabilitation program!",bg="white", font=("times",20 )).place(x=400, y=40)
-        label2 = Label(root, text="Please select an exercise, by moving your hand:",bg="white", font=("times",20 )).place(x=400, y=70)
-        img=PhotoImage(file="exe1.png")
-        img2=PhotoImage(file="exe2.png")
-        canvas.create_image(170, 260, anchor=NW, image=img)
-        canvas.create_image(840,225,anchor=NW,image=img2)
-
-        mainloop()
-
-
+        # Can insert custom code here
+        self.root.update_idletasks()
+        self.root.update()
